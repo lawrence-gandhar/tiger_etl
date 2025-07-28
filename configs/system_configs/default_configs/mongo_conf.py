0eb5af604ed577342247512b5defd32ef82d64f8
Lawrence Gandhar <lawrence_gandhar@yahoo.com>
2025-07-28 22:15:42 +0530

    git commit -m "TIGERETL-14 mongodb config set"

--- configs/system_configs/default_configs/mongo_conf.py
+++ configs/system_configs/default_configs/mongo_conf.py
@@ -1,5 +1,12 @@
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-MONGODB_DB = 'tiger_etl_mongo_db'
-MONGODB_USER = 'admin'
-MONGODB_PASSWORD = '1234'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Loads variables from .env into environment
+
+MONGODB_HOST = os.getenv('MONGODB_HOST', 'localhost')
+MONGODB_PORT = int(os.getenv('MONGODB_PORT', '27017'))
+MONGODB_DB = os.getenv('MONGODB_DB', 'tiger_etl_mongo_db')
+MONGODB_USER = os.getenv('MONGODB_USER', 'admin')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD', '1234')
+
+MONGODB_URI = f"mongodb://{MONGODB_USER}:{MONGODB_PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}/{MONGODB_DB}"
